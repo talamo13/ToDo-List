dc5db5a3e7d4835bc641e3a8bd03caa0b5578552
Tyler <tyleralamo15@gmail.com>
2024-11-21 23:24:08 -0800

    Adding custom user repository method to find documents by username

--- backend/src/main/java/tac/todo/UserController.java
+++ backend/src/main/java/tac/todo/UserController.java
@@ -1,13 +1,16 @@
 package tac.todo;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,5 +22,10 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
        return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Optional<User>> getSingleUser(@PathVariable String name){
+        return new ResponseEntity<Optional<User>>(userService.getUser(name), HttpStatus.OK);
     }
 }
--- backend/src/main/java/tac/todo/UserService.java
+++ backend/src/main/java/tac/todo/UserService.java
@@ -1,9 +1,11 @@
 package tac.todo;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +14,9 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> getUser(String id){
+        return userRepository.findByUsername(id);
     }
 }
