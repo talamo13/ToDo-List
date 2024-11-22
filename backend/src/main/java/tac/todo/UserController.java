694c23fdaa6445e24c0281321c58e0b69605500a
Tyler <tyleralamo15@gmail.com>
2024-11-22 11:09:30 -0800

    Added functionality that allows for api to add and remove tasks from user's task list

--- backend/src/main/java/tac/todo/UserController.java
+++ backend/src/main/java/tac/todo/UserController.java
@@ -16,19 +16,32 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // Get all users in the database
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
        return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
     }
-
+    // Get a specific user via their username
     @GetMapping("/{name}")
     public ResponseEntity<Optional<User>> getSingleUser(@PathVariable String name){
         return new ResponseEntity<Optional<User>>(userService.getUser(name), HttpStatus.OK);
     }
-
+    // Add a new user to the database
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user){
         User savedUser = userService.saveUser(user);
         return new ResponseEntity<User>(savedUser, HttpStatus.OK);
+    }
+    // Adding a task to a user's task list
+    @PutMapping("add/{username}")
+    public ResponseEntity<String> addTask(@RequestBody String task, @PathVariable String username ){
+        userService.addTask(username, task);
+        return ResponseEntity.ok("Task added successfully!");
+    }
+    // Removing a task from a user's task list
+    @PutMapping("remove/{username}")
+    public ResponseEntity<String> removeTask(@RequestBody String task, @PathVariable String username ){
+        userService.removeTask(username, task);
+        return ResponseEntity.ok("Task successfully removed!");
     }
 }
--- backend/src/main/java/tac/todo/UserService.java
+++ backend/src/main/java/tac/todo/UserService.java
@@ -2,6 +2,10 @@ package tac.todo;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,20 +15,38 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
-    public Optional<User> getUser(String id){
+    public Optional<User> getUser(String id) {
         return userRepository.findByUsername(id);
     }
 
-    public User saveUser(User user){
+    public User saveUser(User user) {
         return userRepository.save(user);
     }
 
-    // add task to a users list
+    // Adding a task to a user's task list
+    public void addTask(String username, String task) {
+        // Grabs the appropriate document from the database
+        Query query = new Query(Criteria.where("username").is(username));
+        // Adding the task to the proper document
+        Update update = new Update().push("tasks", task);
+        // Saving our work done
+        mongoTemplate.updateFirst(query, update, User.class);
+    }
 
-    // remove a task from a users list
+    // Remove a task from a user's task list
+    public void removeTask(String username, String task) {
+        // Grabs the appropriate document from the database
+        Query query = new Query(Criteria.where("username").is(username));
+        // Removing the task to the proper document
+        Update update = new Update().pull("tasks", task);
+        // Saving our work done
+        mongoTemplate.updateFirst(query, update, User.class);
+    }
 }
