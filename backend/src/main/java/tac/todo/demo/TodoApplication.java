41b29172acc26471d2d26ea9e844f4220ed28d33
Tyler <tyleralamo15@gmail.com>
2024-11-21 21:46:47 -0800

    Correctly configuring the mongo database

--- backend/src/main/java/tac/todo/demo/TodoApplication.java
+++ backend/src/main/java/tac/todo/demo/TodoApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TodoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String apiRoot(){
-        return "Hello World!";
     }
 
 }
